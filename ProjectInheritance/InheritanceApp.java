4355409e8a4fbbd2b5570143667d2d161d1f8bc5
Hyeonseong Lee <hslee7231@gmail.com>
2022-08-06 23:05:48 +0900

    this & super

--- ProjectInheritance/InheritanceApp.java
+++ ProjectInheritance/InheritanceApp.java
@@ -21,8 +21,9 @@ class Cal {	// Cal 클래스 생성
 	
 	// Overloading, 같은 이름의 메소드를 추가적으로 정의하는 기능(단, 매개변수가 달라야 하고, 자식 클래스에서 정의할 수도 있다.)
 	public int sum(int v1, int v2, int v3) {	//  정수형 매개 변수 세 개를 받는 메소드 생성
-		return v1 + v2 + v3;	// 매개 변수로 받은 인자를 더해서 반환
+		return this.sum(v1, v2) + v3;	// 매개 변수로 받은 인자를 더해서 반환
 	}
+	// this는 클래스 자기 자신을 가르킨다.
 }
 
 class Cal3 extends Cal{	// 클래스 Cal3는 Cal 클래스르 상속 받아 Cal 클래스의 자식 클래스가 됨
@@ -33,8 +34,9 @@ class Cal3 extends Cal{	// 클래스 Cal3는 Cal 클래스르 상속 받아 Cal 
 	// 부모 클래스가 가지고 있던 기능 재정의하는 것을 Overriding이라고 한다.
 	public int sum(int v1, int v2) {	// 부모 클래스에 있던 정수형 매개 변수 두 개를 받는 메소드 생성
 		System.out.println("Cal3!!");	// 구분하기 위해 "Cal3!!"라는 문자열을 출력하게 함
-		return v1 + v2;	// 매개 변수로 받은 인자를 더해서 반환
+		return super.sum(v1, v2);	// 매개 변수로 받은 인자를 더해서 반환
 	}
+	// super는 부모 클래스르 가르킨다.
 	
 	// Overloading, 같은 이름의 메소드를 추가적으로 정의하는 기능(단, 매개변수가 달라야 하고, 자식 클래스에서 정의할 수도 있다.)
 	public int sum(int v1, int v2, int v3) {	//  정수형 매개 변수 세 개를 받는 메소드 생성
