e18634dab484302f457423b8a79475fea1302807
Hyeonseong Lee <hslee7231@gmail.com>
2022-08-06 23:01:09 +0900

    Overriding과 Overloading

--- ProjectInheritance/InheritanceApp.java
+++ ProjectInheritance/InheritanceApp.java
@@ -4,6 +4,7 @@ public class InheritanceApp {
 	public static void main(String[] args) {
 		Cal c = new Cal();	// Cal 클래스를 변수 c에 인스턴스
 		System.out.println(c.sum(2, 1));	// 인스턴스 메소드 sum() 호출
+		System.out.println(c.sum(2, 1, 1));	// 인스턴스 메소드 sum() 호출
 		
 		Cal3 c3 = new Cal3();	// Cal3 클래스를 변수 c3에 인스턴스
 		System.out.println(c3.sum(2, 1));	// Cal3는 Cal의 자식 클래스이기 때문에 부모 클래스의 있는 멤버들도 그대로 상속 받아 따로 정의 및 생성하지 않아도 호출 및 사용이 가능하다.
@@ -17,6 +18,11 @@ class Cal {	// Cal 클래스 생성
 	public int sum(int v1, int v2) {	// 정수형 매개 변수 두 개를 받는 메소드 생성
 		return v1 + v2;	// 매개 변수로 받은 인자를 더해서 반환
 	}
+	
+	// Overloading, 같은 이름의 메소드를 추가적으로 정의하는 기능(단, 매개변수가 달라야 하고, 자식 클래스에서 정의할 수도 있다.)
+	public int sum(int v1, int v2, int v3) {	//  정수형 매개 변수 세 개를 받는 메소드 생성
+		return v1 + v2 + v3;	// 매개 변수로 받은 인자를 더해서 반환
+	}
 }
 
 class Cal3 extends Cal{	// 클래스 Cal3는 Cal 클래스르 상속 받아 Cal 클래스의 자식 클래스가 됨
@@ -28,6 +34,11 @@ class Cal3 extends Cal{	// 클래스 Cal3는 Cal 클래스르 상속 받아 Cal 
 	public int sum(int v1, int v2) {	// 부모 클래스에 있던 정수형 매개 변수 두 개를 받는 메소드 생성
 		System.out.println("Cal3!!");	// 구분하기 위해 "Cal3!!"라는 문자열을 출력하게 함
 		return v1 + v2;	// 매개 변수로 받은 인자를 더해서 반환
+	}
+	
+	// Overloading, 같은 이름의 메소드를 추가적으로 정의하는 기능(단, 매개변수가 달라야 하고, 자식 클래스에서 정의할 수도 있다.)
+	public int sum(int v1, int v2, int v3) {	//  정수형 매개 변수 세 개를 받는 메소드 생성
+		return v1 + v2 + v3;	// 매개 변수로 받은 인자를 더해서 반환
 	}
 }
 
